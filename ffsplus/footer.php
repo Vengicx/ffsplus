36deea033efc971675df25dc998c284957e29f5e
Vengicx <kitocx@gmail.com>
2018-10-30 08:07:33 -0300

    Deletado o ffsplus dentro do ffsplus

--- ffsplus/footer.php
+++
@@ -1,5 +0,0 @@
-<strong>Faster Food Service &copy; 2018 -</strong>
-    Todos os Direitos Reservados. Developed with <i class="fa fa-heart"></i> by Giovanni Bússola
-<div class="float-right d-none d-sm-inline-block">
-    <b>Versão</b> 1.0.3 - <a style="color: inherit;" href="https://github.com/Vengicx/ffsplus"><b>Vengicx7 </b><i class="fa fa-github"></i></a>
-</div>
